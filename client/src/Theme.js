a60738e7cddc80b5cbc45b67b9868dfa7683c25d
vlntnwest <152171895+vlntnwest@users.noreply.github.com>
2024-10-02 14:14:48 +0200

    Font

--- client/src/Theme.js
+++ client/src/Theme.js
@@ -1,6 +1,5 @@
 // src/theme.js
 import { createTheme } from "@mui/material/styles";
-import RobotoCondensed from "./assets/fonts/RobotoCondensed-VariableFont_wght.ttf";
 
 const theme = createTheme({
   palette: {
@@ -51,11 +50,11 @@ const theme = createTheme({
   components: {
     MuiCssBaseline: {
       styleOverrides: `
-        @font-face {
-          font-family: 'Roboto Condesed';
-          src: local('Roboto Condesed'), local('Roboto-Condesed-Regular'), url(${RobotoCondensed}) format('ttf');
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-        }
+       @font-face {
+        font-family: 'Roboto Condensed';
+        src: url('/assets/fonts/RobotoCondensed-VariableFont_wght.ttf') format('truetype');
+        unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+      }
           html,
           body {
             overscroll-behavior: none; 
