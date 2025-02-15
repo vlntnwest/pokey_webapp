c16eb0c0317550a8c908e146e9db67ee2a2bba6c
Valentin WESTERMEYER <valentin@Mac-mini-de-Valentin.local>
2025-02-15 12:44:04 +0100

    cors headers

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ const app = express();
 const corsOption = {
   origin: process.env.CLIENT_URL,
   credentials: true,
-  allowedHeaders: ["sessionId", "Content-Type"],
+  allowedHeaders: [
+    "sessionId",
+    "Content-Type",
+    "Content-Type",
+    "Authorization",
+  ],
   exposedHeaders: ["sessionId"],
   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
   preflightContinue: false,
