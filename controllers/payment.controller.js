d97fddd6ad90b9f6b99cc24c028ae2a9651a2ec0
Valentin WESTERMEYER <valentin@westermeyer.fr>
2025-04-11 22:53:35 +0200

    minimum stripe payment

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -6,46 +6,56 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
 });
 
 module.exports.createCheckoutSession = async (req, res) => {
-  const { email, stripeItems, data } = req.body;
+  try {
+    const { email, stripeItems, data, totalPrice } = req.body;
 
-  function splitStringToMetadataParts(key, value, chunkSize = 490) {
-    const chunks = value.match(new RegExp(`.{1,${chunkSize}}`, "g")) || [];
-    return chunks.reduce((acc, chunk, i) => {
-      acc[`${key}_part_${i + 1}`] = chunk;
-      return acc;
-    }, {});
-  }
-
-  const formattedMetadata = {};
-
-  for (const [key, value] of Object.entries(data)) {
-    if (typeof value === "object") {
-      const jsonValue = JSON.stringify(value);
-      if (jsonValue.length > 490) {
-        Object.assign(
-          formattedMetadata,
-          splitStringToMetadataParts(key, jsonValue)
-        );
-      } else {
-        formattedMetadata[key] = jsonValue;
-      }
-    } else {
-      formattedMetadata[key] = String(value);
+    if (totalPrice < 0.5) {
+      return res.status(400).json({ error: "Montant minimum requis : 0.50 €" });
     }
+
+    function splitStringToMetadataParts(key, value, chunkSize = 490) {
+      const chunks = value.match(new RegExp(`.{1,${chunkSize}}`, "g")) || [];
+      return chunks.reduce((acc, chunk, i) => {
+        acc[`${key}_part_${i + 1}`] = chunk;
+        return acc;
+      }, {});
+    }
+
+    const formattedMetadata = {};
+
+    for (const [key, value] of Object.entries(data)) {
+      if (typeof value === "object") {
+        const jsonValue = JSON.stringify(value);
+        if (jsonValue.length > 490) {
+          Object.assign(
+            formattedMetadata,
+            splitStringToMetadataParts(key, jsonValue)
+          );
+        } else {
+          formattedMetadata[key] = jsonValue;
+        }
+      } else {
+        formattedMetadata[key] = String(value);
+      }
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: stripeItems,
+      metadata: formattedMetadata,
+      customer_email: email,
+      mode: "payment",
+      payment_method_types: ["card"],
+      ui_mode: "custom",
+      return_url: process.env.CLIENT_URL,
+    });
+
+    res.json({ checkoutSessionClientSecret: session.client_secret });
+  } catch (err) {
+    console.error("Erreur Stripe Checkout:", err);
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la création de la session de paiement." });
   }
-
-  const session = await stripe.checkout.sessions.create({
-    line_items: stripeItems,
-    metadata: formattedMetadata,
-    customer_email: email,
-    mode: "payment",
-    payment_method_types: ["card"],
-    ui_mode: "custom",
-    // The URL of your payment completion page
-    return_url: process.env.CLIENT_URL,
-  });
-
-  res.json({ checkoutSessionClientSecret: session.client_secret });
 };
 
 module.exports.paymentWebhook = async (req, res) => {
