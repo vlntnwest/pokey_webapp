de9668f02cb6ba6b0dd35eb15fe7e3f8af20c911
vlntnwest <valentin@westermeyer.fr>
2026-01-27 18:52:12 +0100

    prisma schema

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const supabase = require("../lib/supabase");
+const prisma = require("../lib/prisma");
 
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
@@ -21,6 +22,19 @@ router.post("/signup", async (req, res) => {
     }
 
     res.status(200).json({ data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error: " + error.message });
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.public_users.findUnique({
+      where: { id },
+    });
+    res.status(200).json({ user });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error: " + error.message });
